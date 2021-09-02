0174aafb4dc0b853817e9ab04ca03318280e1875
Eduard-Konovka <ed098ua@gmail.com>
2021-09-02 21:28:32 +0300

    Добавляет константы

--- src/js/to-change-theme.js
+++ src/js/to-change-theme.js
@@ -1,11 +1,16 @@
 import refs from './refs'
 
+const THEME = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+}
+
 if (!localStorage.getItem('class')) {
-  localStorage.setItem('class', 'light-theme')
-  refs.body.classList.add('light-theme')
+  localStorage.setItem('class', THEME.LIGHT)
+  refs.body.classList.add(THEME.LIGHT)
 } else {
   refs.body.classList.add(localStorage.getItem('class'))
-  if (localStorage.getItem('class') === 'light-theme') {
+  if (localStorage.getItem('class') === THEME.LIGHT) {
     refs.inputCheckbox.checked = false
   } else {
     refs.inputCheckbox.checked = true
@@ -13,11 +18,11 @@ if (!localStorage.getItem('class')) {
 }
 
 refs.inputCheckbox.addEventListener('change', () => {
-  if (localStorage.getItem('class') === 'light-theme') {
-    localStorage.setItem('class', 'dark-theme')
-    refs.body.classList.replace('light-theme', 'dark-theme')
+  if (localStorage.getItem('class') === THEME.LIGHT) {
+    localStorage.setItem('class', THEME.DARK)
+    refs.body.classList.replace(THEME.LIGHT, THEME.DARK)
   } else {
-    localStorage.setItem('class', 'light-theme')
-    refs.body.classList.replace('dark-theme', 'light-theme')
+    localStorage.setItem('class', THEME.LIGHT)
+    refs.body.classList.replace(THEME.DARK, THEME.LIGHT)
   }
 })
